548455a7ae7bb7e6b6426aaf6c6d76773ab9046f
johanrhodin <johan.rhodin@gmail.com>
2017-08-22 11:28:38 +0200

    Add date

--- main.exs
+++ main.exs
@@ -1,6 +1,6 @@
 {:ok, connection} = AMQP.Connection.open(System.get_env("CLOUDAMQP_URL"))
 {:ok, channel} = AMQP.Channel.open(connection)
-temphumidity = System.cmd("sudo", ["/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py", "22", "4"]) 
+temphumidity = inspect(Tuple.to_list(:calendar.local_time())) <> " " <> System.cmd("sudo", ["/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py", "22", "4"]) 
 AMQP.Queue.declare(channel, "temperaturePub")
 AMQP.Basic.publish(channel, "", "temperaturePub", to_string(temphumidity))
 IO.puts " [x] Sent "<> to_string(temphumidity)
