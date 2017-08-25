e988c566d7ea31c4a99e9cebb8cabb8ab7ef812a
johanrhodin <johan.rhodin@gmail.com>
2017-08-25 15:02:03 +0200

    Add time keyword

--- main.exs
+++ main.exs
@@ -1,6 +1,6 @@
 {:ok, connection} = AMQP.Connection.open(System.get_env("CLOUDAMQP_URL"))
 {:ok, channel} = AMQP.Channel.open(connection)
-temphumidity = inspect(Tuple.to_list(:calendar.local_time())) <> " " <> elem(System.cmd("sudo", ["/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py", "22", "4"]),0)
+temphumidity = "Time=" <> inspect(Tuple.to_list(:calendar.local_time())) <> " " <> elem(System.cmd("sudo", ["/home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py", "22", "4"]),0)
 AMQP.Queue.declare(channel, "temperaturePub", durable: true)
 AMQP.Basic.publish(channel, "", "temperaturePub", to_string(temphumidity), persistent: true)
 # IO.puts " [x] Sent "<> to_string(temphumidity)
